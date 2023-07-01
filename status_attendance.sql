0389d4544d009b1929c62a138da82d0f5eb9deb4
Maksim <email@mail.ru>
2023-07-01 16:32:05 +0400

    AB-6 заполнения бд данными

--- status_attendance.sql
+++ status_attendance.sql
@@ -5,3 +5,8 @@ CREATE TABLE accounting_bd.`status_attendance` (
     CONSTRAINT status_attendance_pk PRIMARY KEY (id),
     CONSTRAINT status_attendance_un UNIQUE KEY (name)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_general_ci;
+
+INSERT INTO accounting_bd.status_attendance (name, description) VALUES('О', 'Отпуск');
+INSERT INTO accounting_bd.status_attendance (name, description) VALUES('П', 'Прогул');
+INSERT INTO accounting_bd.status_attendance (name, description) VALUES('Р', 'Работал');
+INSERT INTO accounting_bd.status_attendance (name, description) VALUES('Б', 'Болен');
--- status_worker.sql
+++ status_worker.sql
@@ -5,3 +5,8 @@ CREATE TABLE accounting_bd.`status_worker` (
     CONSTRAINT status_worker_pk PRIMARY KEY (id),
     CONSTRAINT status_worker_un UNIQUE KEY (name)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_general_ci;
+
+INSERT INTO accounting_bd.status_worker (name, description) VALUES('Р', 'Работающий');
+INSERT INTO accounting_bd.status_worker (name, description) VALUES('У', 'Уволен');
+INSERT INTO accounting_bd.status_worker (name, description) VALUES('О', 'Отпуск');
+INSERT INTO accounting_bd.status_worker (name, description) VALUES('П', 'Пенсия');
