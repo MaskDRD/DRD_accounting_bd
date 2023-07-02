a5266a2c28ac86fc66709fe876707ecd54f716ed
Maksim <email@mail.ru>
2023-07-02 09:47:50 +0400

    AB-10 создание хранимых процедур на стороне БД

--- worker.sql
+++ worker.sql
@@ -12,3 +12,19 @@ CREATE TABLE `worker` (
     CONSTRAINT `worker_status_FK` FOREIGN KEY (`id_status_worker`) REFERENCES `status_worker` (`id`),
     CONSTRAINT `worker_position_FK` FOREIGN KEY (`id_position`) REFERENCES `position` (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+
+CREATE PROCEDURE accounting_bd.worker_get()
+begin
+	SELECT 
+		w.id, 
+		w.surname, 
+		w.name, 
+		w.patronymic, 
+		w.zp,
+		p.name as p_name, 
+		s_w.name as s_w_name
+		FROM worker w
+		LEFT JOIN position p on p.id = w.id_position
+		LEFT JOIN status_worker s_w on s_w.id = w.id_status_worker;
+END
