35b7ec9f7ecb70505be094972f277f3e0f5423f4
Maksim <email@mail.ru>
2023-07-15 23:27:50 +0400

    перенос файлов

--- position.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE accounting_bd.`position` (
-    id INT auto_increment NOT NULL,
-    name varchar(100) NOT NULL,
-    CONSTRAINT position_pk PRIMARY KEY (id),
-    CONSTRAINT position_un UNIQUE KEY (name)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_general_ci;
