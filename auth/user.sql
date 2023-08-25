b1e91a324575fb6f4bc21de30e4a962518b205be
Maksim <email@mail.ru>
2023-08-25 08:26:01 +0400

    удаление nik из user

--- auth/user.sql
+++ auth/user.sql
@@ -3,13 +3,11 @@ CREATE TABLE users(
 	login varchar(128),
 	password varchar(256),
 	email varchar(128),
-	nik varchar(128),
 	check_active boolean default 1,
 	check_conf_email boolean default 0,
 	CONSTRAINT customers_pk PRIMARY KEY(id),
 	CONSTRAINT customers_login_uq UNIQUE KEY(login),
 	CONSTRAINT customers_email_uq UNIQUE KEY(email),
-	CONSTRAINT customers_nik_uq UNIQUE KEY(nik)
 );
 
 -- bd.UserGetWhereTokenValue
@@ -20,7 +18,6 @@ begin
 		u.id,
 		u.login,
 		u.email,
-		u.nik,
 		u.check_active,
 		u.check_conf_email,
 		t.id as token_id, 
@@ -40,7 +37,6 @@ begin
 		u.id,
 		u.login,
 		u.email,
-		u.nik,
 		u.check_active,
 		u.check_conf_email,
 		t.id as token_id, 
@@ -60,7 +56,6 @@ begin
 		u.id,
 		u.login,
 		u.email,
-		u.nik,
 		u.check_active,	
 		u.check_conf_email
 	from users u
@@ -73,26 +68,23 @@ DROP PROCEDURE IF EXISTS bd.UserCheck;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `bd`.`UserCheck`(
 	in _login varchar(128),
 	in _email varchar(128),
-	in _nik varchar(128),
 	out check_login_ boolean,
-	out check_email_ boolean,
-	out check_nik_ boolean
+	out check_email_ boolean
 )
 begin
 	select count(u.login) into check_login_ from users u where u.login = _login limit 1;
 	select count(u.email) into check_email_ from users u where u.email = _email limit 1;
-	select count(u.nik) into check_nik_ from users u where u.nik = _nik limit 1;
 end
+
 -- bd.UserCreate
 DROP PROCEDURE IF EXISTS bd.UserCreate;
 CREATE DEFINER = `root` @`localhost` PROCEDURE bd.UserCreate(
 	in _login varchar(128),
 	in _password varchar(256),
-	in _email varchar(128),
-	in _nik varchar(128)
+	in _email varchar(128)
 ) 
 begin
-	INSERT users(login, password, email, nik)
-	VALUES (_login, _password, _email, _nik);
+	INSERT users(login, password, email)
+	VALUES (_login, _password, _email);
 	select LAST_INSERT_ID() as id;
 end
